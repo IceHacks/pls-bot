daecc05e48a006bcd1a9c48e31ed55911af4e2de
IceHacks <38994479+IceHacks@users.noreply.github.com>
2019-11-15 10:20:20 -1000

    update

---
+++ build.js
@@ -0,0 +1,38 @@
+const builder = require("electron-builder");
+const Platform = builder.Platform;
+const token = process.env.TOKEN;
+const gh = require("gh-release");
+
+console.log(token);
+
+builder
+	.build({
+		targets: Platform.MAC.createTarget(),
+		config: {
+			appId: "com.pls.bot",
+			productName: "PlsBot",
+			copyright: "© 2019-20 IceHacks",
+			directories: {},
+			mac: {
+				electronLanguages: []
+			},
+			artifactName: "plsbot-built.${ext}"
+		}
+	})
+	.then(() => {
+		gh(
+			{
+				tag_name: "v1.0.0",
+				name: "Travis Automated Build",
+				owner: "IceHacks",
+				repo: "pls-bot",
+				assets: ["plsbot-built.dmg"],
+				auth: token
+			},
+			(err, result) => {
+				if (err) throw err;
+
+				console.log(result);
+			}
+		);
+	});
