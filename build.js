0edeb5568591c906bac9bb77e1fb33d632e9be32
IceHacks <38994479+IceHacks@users.noreply.github.com>
2019-11-19 18:12:17 -1000

    update

--- build.js
+++ build.js
@@ -1,6 +1,8 @@
 const builder = require("electron-builder");
 const Platform = builder.Platform;
 const token = process.env.GH_TOKEN;
+const fs = require("fs-extra");
+const path = require("path");
 
 console.log(token);
 
@@ -17,4 +19,20 @@ builder
 			artifactName: "plsbot-built.${ext}"
 		}
 	})
-	.then(() => {});
+	.then(() => {
+		fs.readdir("./dist/", (err, files) => {
+			if (err) {
+				console.log(err);
+			}
+
+			files.forEach(file => {
+				const fileDir = path.join("./dist/", file);
+
+				if (!file.match(/plsbot-built\.(\w+)$/g)) {
+					try {
+						fs.removeSync(fileDir);
+					} catch (e) {}
+				}
+			});
+		});
+	});
